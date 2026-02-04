d6c866a2627c9333d366429ea0cd06b7fbfc8bf4
23318dp <23318dp@hvhs.school.nz>
2026-02-04 15:04:53 +1300

    text input

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function start() {
-alert("Congratulations!\nYou got the script running.")
+ document.getElementById("welcomeMessage").innerHTML = "Hello World";
 }
 
+function useInput() {
+  const name = document.getElementById("nameInput").value;
+  document.getElementById("welcomeMessage").innerHTML =
+    "Hello " + name;
+}
